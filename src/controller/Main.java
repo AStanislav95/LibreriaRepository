876ade6450fb6650d2a585dfbdeea5e4f016ae4d
dangeloandrea14 <37885084+dangeloandrea14@users.noreply.github.com>
2018-11-12 13:26:49 +0100

    TI PREGO DIO FUNZIONA TI PREGO

---
+++ src/controller/Main.java
@@ -0,0 +1,14 @@
+package controller;
+import model.Pagina;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Pagina p=new Pagina();
+		p.setManoscritto("Divina Commedia");
+		System.out.println(p.getManoscritto());
+		
+	}
+	
+}
+

bede6ff045efd395c995bc66ae55781762df20a7
Antokhi Stanislav <antokhi.stas@gmail.com>
2018-11-12 16:35:47 +0100

    asd

--- src/model/ObjectContenitor.java
+++ src/model/ObjectContenitor.java
@@ -4,20 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ObjectContenitor {
+	
 	public static Utente utenteAttivo = new Utente();
 	public static ArrayList<Manoscritto> listaManoscritti = new ArrayList<Manoscritto>();
 
-	
-	public void addManoscritto(Manoscritto m) { 
-		listaManoscritti.add(m);
-	}
-	
-	
-	public ArrayList<Manoscritto> getListaManoscritti(){
-		return listaManoscritti;
-	}
-	
-	
-	
 	
 }//end method

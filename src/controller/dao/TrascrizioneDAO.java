73f6485b701ff3545d628ecd4a3097123f1fb1cf
Antokhi Stanislav <antokhi.stas@gmail.com>
2018-11-13 09:48:23 +0100

    Trascrizione DAO

--- src/controller/dao/TrascrizioneDAO.java
+++ src/controller/dao/TrascrizioneDAO.java
@@ -1,5 +1,22 @@
 package controller.dao;
 
-public class TrascrizioneDAO {
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TrascrizioneDAO {
+	
+	
+	public static ResultSet getTrascrizioni() throws Exception {
+		Connection con = ConnectionDAO.getConnection();
+		Statement stm = con.createStatement();
+		ResultSet rs = stm.executeQuery("SELECT * FROM Trascrizione");
+		return rs;
+	}
+	
+	
+	
+	
 }

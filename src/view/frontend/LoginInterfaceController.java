8177f7b7dd6ceb1674eab056aac1184a00d2f6f6
Stas <awakenedgenius95@gmail.com>
2018-11-13 20:15:34 +0100

    Nuovi package

--- src/view/frontend/LoginInterfaceController.java
+++ src/view/frontend/LoginInterfaceController.java
@@ -1,16 +1,52 @@
 package view.frontend;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.stage.Stage;
 
-public class LoginInterfaceController {
 
+public class LoginInterfaceController implements Initializable{
+
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button register;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    private Label nameerror;
+    
+    @FXML
+    private Label passerror;
+    
+    @FXML
+    private Label errorelogin;
+    
+    @FXML
+    private TextField passtext;
+    
+    @FXML
+    private Button chiudi;
+    
+    @FXML
+    private CheckBox checkpassword;
 	
 	@FXML
 	private void chiudi(ActionEvent e) {
@@ -19,7 +55,8 @@ public class LoginInterfaceController {
 	
 	@FXML
 	private void login() {
-
+		//getUserData(String name, String password)
+		//DAO ->
 	}
 	
 	@FXML
@@ -38,6 +75,12 @@ public class LoginInterfaceController {
 	
 	@FXML
 	private void check(ActionEvent e) {
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
 		
 	}
 }

946d0b9ac26eb4d0cb07296587304d99fbc237f7
dangeloandrea14 <37885084+dangeloandrea14@users.noreply.github.com>
2018-11-13 15:23:50 +0100

    DownloadController (?)

--- src/controller/viewer/DownloadController.java
+++ src/controller/viewer/DownloadController.java
@@ -1,5 +1,31 @@
 package controller.viewer;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
 public class DownloadController {
 
+	
+	/*private void download(int id){
+		FileChooser fileChooser = new FileChooser();
+		
+		fileChooser.setTitle("Scegli dove salvare");
+		
+		File file = fileChooser.showSaveDialog(appEntry.getStage());
+		if(file != null){
+		    try{
+			ImageIO.write(SwingFXUtils.fromFXImage(selectedImage.getImage(),
+	                        null), "png", file);
+		    }catch(IOException ex){
+			System.out.println(ex.getMessage());
+		    }
+		}
+	    }*/ 
+	//Da aggiustare poi	
 }

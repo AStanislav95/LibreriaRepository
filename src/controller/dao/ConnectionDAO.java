20ec2c1af96fd7c44d3517986467d0a084e150f5
Antokhi Stanislav <antokhi.stas@gmail.com>
2018-11-15 13:42:23 +0100

    ASFWE

--- src/controller/dao/ConnectionDAO.java
+++ src/controller/dao/ConnectionDAO.java
@@ -7,52 +7,52 @@ import java.sql.Statement;
 
 
 public class ConnectionDAO {
-	private static String URL = "jdbc:mysql://localhost:3306/libreria?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-	
-	private static String username = "root";
-	private static String pass = "";
+  private static String URL = "jdbc:mysql://localhost:3306/libreria?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+  
+  private static String username = "root";
+  private static String pass = "root";
 
    
-	
-	public static Connection getConnection() throws Exception  {
+  
+  public static Connection getConnection() throws Exception  {
         Connection conn = null;
         conn = DriverManager.getConnection(URL,username, pass);
              
         return conn;
     }
 
-	public static void closeConnection(ResultSet rs, Connection conn, Statement stat) throws Exception{
-		if(rs !=null) {
-			rs.close();
-		}
-		if(conn!=null) {
-			conn.close();
-		}	
-		if(stat!=null) {
-			stat.close();
-		}
-		
-	}
-	
-	
-	
-	public static ResultSet query(String str) {
-		try {
+  public static void closeConnection(ResultSet rs, Connection conn, Statement stat) throws Exception{
+    if(rs !=null) {
+      rs.close();
+    }
+    if(conn!=null) {
+      conn.close();
+    }  
+    if(stat!=null) {
+      stat.close();
+    }
+    
+  }
+  
+  
+  
+  public static ResultSet query(String str) {
+    try {
 
-		    
-		    Connection con = DriverManager.getConnection(URL,username, pass);
-		    Statement stm = con.createStatement();
-		    ResultSet result = stm.executeQuery(str);
+        
+        Connection con = DriverManager.getConnection(URL,username, pass);
+        Statement stm = con.createStatement();
+        ResultSet result = stm.executeQuery(str);
 
-		    return result;
+        return result;
 
-		} catch (Exception e) {
+    } catch (Exception e) {
 
-		    System.out.println(e.getMessage());
-		    return null;
-		}
-	 
-	}
-	
-	
+        System.out.println(e.getMessage());
+        return null;
+    }
+   
+  }
+  
+  
 }

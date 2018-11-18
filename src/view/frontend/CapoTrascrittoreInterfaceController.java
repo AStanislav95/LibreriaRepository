1caf2f7328ce25c2afd26e4b0a6877d69e9aeaf3
Antokhi Stanislav <antokhi.stas@gmail.com>
2018-11-18 11:28:26 +0100

    Capotrascrittore

--- src/view/frontend/CapoTrascrittoreInterfaceController.java
+++ src/view/frontend/CapoTrascrittoreInterfaceController.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import controller.dao.PaginaDAO;
+import controller.dao.TrascrizioneDAO;
 import controller.transcriber.TrascrizioneEditorController;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -29,24 +30,23 @@ import model.Pagina;
 
 public class CapoTrascrittoreInterfaceController implements Initializable {
 
-	static int idManoscritto;
-
-	static int idPagina;
 	@FXML
 	private Button Homepage;
 	@FXML
 	private Button submit;
 
 	@FXML
-	private ListView<Integer> pagina;
+	private ListView<Integer> idTrasc;
 	@FXML
 	private ImageView img;
 	@FXML
 	private HTMLEditor editor;
-
-	private ObservableList<Integer> idPagine = FXCollections.observableArrayList();
-	private ObservableList<Pagina> pagine = FXCollections.observableArrayList();
+	
 	private static int ind;
+	private ObservableList<Integer> idTrascrizioni = FXCollections.observableArrayList();
+	private ObservableList<String> tras = FXCollections.observableArrayList();
+	private ObservableList<String> scanPathTrascrizioni = FXCollections.observableArrayList();
+	
 
 	@FXML
 	private void back(ActionEvent e) throws Exception {
@@ -57,60 +57,60 @@ public class CapoTrascrittoreInterfaceController implements Initializable {
 	@FXML
 	private void submit(ActionEvent e) throws Exception {
 
-		idPagine.remove(ind);
-
-		pagina.refresh();
-
-		String text = getText(editor.getHtmlText());
-
-		// -----------Non funziona----------
-		TrascrizioneEditorController.insertTrascrizione(idPagina, text, ObjectContenitor.utenteAttivo.getID());
-		// ------------ !! ----------
-		editor.setHtmlText("");
+//		idPagine.remove(ind);
+//
+//		pagina.refresh();
+//
+//		String text = getText(editor.getHtmlText());
+//
+//		// -----------Non funziona----------
+//		TrascrizioneEditorController.insertTrascrizione(idPagina, text, ObjectContenitor.utenteAttivo.getID());
+//		// ------------ !! ----------
+//		editor.setHtmlText("");
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
-		ResultSet pagineAssegnate = PaginaDAO.pagineAssegnate(ObjectContenitor.utenteAttivo.getID());
-
+		ResultSet trascrizioni = TrascrizioneDAO.getTrascrizioniDaRevisionare();
+		
 		try {
-			while (pagineAssegnate.next()) {
-				idPagine.add(pagineAssegnate.getInt(1));
-				pagine.add(new Pagina(pagineAssegnate.getInt(1), pagineAssegnate.getString(2)));
+			while(trascrizioni.next()) {
+				
+				idTrascrizioni.add(trascrizioni.getInt(1));
+				tras.add(trascrizioni.getString(2));
+				scanPathTrascrizioni.add(trascrizioni.getString(5));
+				
 			}
 		} catch (SQLException e) {
-
+			
 			e.printStackTrace();
 		}
-
-		pagina.setItems(idPagine);
-		pagina.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-		pagina.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
+		
+		idTrasc.setItems(idTrascrizioni);
+		idTrasc.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		idTrasc.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
 
 			@Override
 			public void changed(ObservableValue<? extends Integer> arg0, Integer arg1, Integer arg2) {
-
-				ind = pagina.getSelectionModel().getSelectedIndex();
-				idPagina = arg0.getValue();
-
-				for (Pagina p : pagine) {
-					if (p.getID() == arg0.getValue()) {
-
-						try {
-							img.setImage(new Image(new FileInputStream(p.getScanpath())));
-						} catch (FileNotFoundException e) {
-
-							e.printStackTrace();
-						}
-					}
+				
+				ind = idTrasc.getSelectionModel().getSelectedIndex();
+				
+				try {
+					
+					img.setImage(new Image(new FileInputStream(scanPathTrascrizioni.get(ind))));
+					editor.setHtmlText(tras.get(ind));
+					
+				} catch (FileNotFoundException e) {
+				
+					e.printStackTrace();
 				}
-
+				
 			}
-
+			
 		});
 
-	}
+	}//end init
 
 	public static String getText(String htmlText) {
 
